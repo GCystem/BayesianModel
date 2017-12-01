c89dcc2c34596649540471303b91d1e084bd2814
GCystem <georgey.chen36@gmail.com>
2017-12-01 00:12:57 -0800

    beta upload

--- bay.py
+++ bay.py
@@ -14,68 +14,51 @@ V = np.array([]) # store INPUT
 U = np.array([]) # store OUTPUT
 
 def loadCSV(file, X, Y):
-	data = np.loadtxt(open(file, "rb"), dtype = 'float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float', delimiter=",", skiprows=1, usecols=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
+	data = np.loadtxt(open(file, "rb"), dtype = 'float, float, float, float, float, float, float, float, float, float', delimiter=",", skiprows=1, usecols=(0,1,2,3,4,5,6,7,8,9))
 	global V,U
 	t = 0
 	for row in data:
 			# if t < 50:
 		temp = np.array([])
-		for i in range(0,13):
+		for i in range(0,9):
 			temp = np.append(temp, row[i])
 		
 		X = np.vstack((X,temp))
-		Y = np.append(Y, row[15])
+		Y = np.append(Y, row[9])
 			# t += 1
 
 	V = X
 	U = Y
 
-	# with open(file, 'rb') as csvfile:
-	# 	reader = csv.DictReader(csvfile)
-	# 	t = 0
-	# 	for row in reader:
-	# 		if t < 50:
-	# 			# print(row['Value at Median $ per Sq. Ft.(2017)'])
-	# 			T = []
-	# 			T.append(float(row['Lat']))
-	# 			T.append(float(row['Lng']))
-	# 			T.append(float(row['Tax rate area']))
-	# 			# T.append(float(row['Neighborhood']))
-	# 			# T.append(float(row['Lot sq. ft.']))
-	# 			# T.append(float(row['Property class']))
-	# 			# T.append(float(row['Year built']))
-	# 			# T.append(float(row['Square feet']))
-	# 			# T.append(float(row['Rooms']))
-	# 			T.append(float(row['Bedrooms']))
-	# 			T.append(float(row['Bathrooms']))
-	# 			T.append(float(row['ZIP code']))
-	# 			print(T)
-	# 			T = np.array(X)
-	# 			X.append(T)
-	# 			Y.append(float(row['Value at Median $ per Sq. Ft.(2017)']))
-	# 		t += 1
-
 @app.route('/train', methods = ['GET'])
 def train():
-	X = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12])
+	X = np.array([0,1,2,3,4,5,6,7,8])
 	Y = np.array([0])
 	print("TRAINING")
-	loadCSV('test.csv', X, Y)
-
-	# print(V)
-	# print(U)
+	loadCSV('test3.csv', X, Y)
 
 	clf = linear_model.BayesianRidge()
 	clf.fit(V,U)
-	res = clf.predict([[0, 37.3084232, -121.8951796, 17108.0, 0.0, 13200.0, 0.0, 1900.0, 1327.0, 8.0, 4.0, 2.0, 95125.0]])
-
+	# res = clf.predict([[0, 37.3084232, -121.8951796, 17108.0, 0.0, 13200.0, 0.0, 1900.0, 1327.0, 8.0, 4.0, 2.0, 95125.0]])
+	# res = clf.predict([[37.3084232, -121.8951796, 0.0, 13200.0, 1900.0, 1327.0, 8.0, 4.0, 2.0, 713.0]])
+	# res = clf.predict([[37.3084232, -121.8951796, 0.0, 13200.0, 1900.0, 1327.0, 8.0, 4.0, 2.0, 713.0, 2015]])
+	# res = clf.predict([[37.3083563,-121.8948274,0,8500,1988,2371,7,3,2.5,713,2005]])
+	res = clf.predict([[37.297955,-121.812944,0,1972,1799,4,2,95131,2017]])
+	clf.
 	print(res[0])
+
+	###### SAVING MODEL
+	pickle.dump(clf, open('trained_model.sav', 'wb'))
+	loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+	print(loaded_model.predict([[37.297955,-121.812944,0,1972,1799,4,2,95131,2017]]))
+	# print(loaded_model.predict([[37.3085298,-121.895274,0,12200,1947,1575,7,3,2,713,2011]]))
 	return jsonify(result = res[0])
 
 	################### SAVE/LOAD Model ################################
 	# pickle.dump(clf, open('trained_model.sav', 'wb'))
 	# loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 	# print(loaded_model.predict([[0, 37.3084232, -121.8951796,17108.0, 0.0, 13200.0, 0.0, 1900.0, 1327.0, 8.0, 4.0, 2.0, 95125.0]]))
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=1314, debug=True)
---
+++ bay_rest.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pickle
+from flask import Flask, request, Response, json
+from flask import jsonify
+
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+app = Flask(__name__)
+
+@app.route('/Bay', methods = ['POST'])
+def train():
+    p_body = json.loads(request.data)
+    lat = float(p_body["lat"])
+    lng = float(p_body["lng"])
+    property_class = float(p_body["property_class"])
+    year_built = float(p_body['year_built'])
+    square_feet = float(p_body['square_feet'])
+    bedrooms = float(p_body['bedrooms'])
+    bathrooms = float(p_body['bathrooms'])
+    zip_code = float(p_body['zip_code'])   
+    transaction_year = float(p_body['transaction_year'])  
+
+    res = loaded_model.predict([[lat, lng, property_class, year_built, square_feet, bedrooms, bathrooms, zip_code, transaction_year]])
+    return jsonify(lat = lat, lng = lng, property_class = property_class, year_built = year_built, square_feet = square_feet, 
+                    bedrooms = bedrooms, bathrooms = bathrooms, zip_code = zip_code, transaction_year = transaction_year, price = res[0])
+
+if __name__ == '__main__':
+
+	app.run(host='0.0.0.0', port=10002, debug=True)
+	
